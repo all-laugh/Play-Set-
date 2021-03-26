005399183e81dbf87ed341d52db5047f74f15766
all-laugh <xq264@nyu.edu>
2021-03-26 17:50:03 -0400

    Initial Commit

---
+++ Play Set!/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Play Set!
+//
+//  Created by Xiao Quan on 3/26/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
